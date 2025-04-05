7781219506806968ae5dcb472c73e67b3d07087a
joao simoes <joao.simoes@tecnico.ulisboa.pt>
2025-04-05 01:17:51 +0100

    fix server url

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/', name: 'HomeView', component: HomeView,
+      path: '/joao.simoes/', name: 'HomeView', component: HomeView,
     },
 
   ],
